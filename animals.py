578c38e7912d35e1017691e6a4ea2c2e593a2aaf
Alex Martin <alex@subtleco.co>
2021-04-16 10:18:35 -0500

    initial commit

---
+++ animals.py
@@ -0,0 +1,116 @@
+from datetime import date
+
+class Llama():
+    def __init__(self, name, species, shift, food):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True,
+        self.shift = shift,
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+
+
+class Goat():
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class Tiger():
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class Lemur():
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class Human():
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class Cobra():
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+        
+class Cottonmouth():
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+        
+class Rattlesnake():
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+        
+class Garter_Snake():
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+        
+class Slimy():
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+        
+class Clownfish():
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+        
+class Marlin():
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+        
+class Dolphin():
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+        
+class Whale():
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+        
+class Turtle():
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+danny = Llama("Danny", "Alpaca", "Shift B", "Llama food")
+
+print(danny.feed())
