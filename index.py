f1a62e0e8cadada731cd1a2a3f2008ca05a9e3b1
Alex Martin <alex@subtleco.co>
2021-04-17 13:36:30 -0500

    enough to be done with this book

--- index.py
+++ index.py
@@ -1,12 +1,15 @@
 from models import (Llama,
                     Petting_Zoo,
-                    Human)
+                    Human,
+                    Garter_Snake)
 
 danny = Llama("Danny", "Alpaca", "Shift B", "Llama food", 12345)
 zoo = Petting_Zoo("Big Zoo", "A place for fuzzies")
 alex = Human("Alex", "Regular Human", "Shift A", "Waffles", 666)
 zoo.admit(danny)
 zoo.admit(alex)
+ekans = Garter_Snake("Ekans", "CrazySnake", "Shift B", "mice", 32323)
+zoo.admit(ekans)
 zoo.roster()
 
 
